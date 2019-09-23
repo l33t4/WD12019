abed7377f399f5ca0e7f30dfa71e5de42f008635
Thomas Devine <tomo@Thomass-iMac.local>
2019-09-23 10:01:07 +0100

    added status codes

--- restapi/restapiONE/index.js
+++ restapi/restapiONE/index.js
@@ -14,19 +14,23 @@ app.use(bodyParser.urlencoded({extended:false}));
 // setup REST API /team route VERBS - GET, POST, PUT, DELETE
 app.route('/team/:id?')
   .get(function (req, res) {  
+    res.status(200);  // 200 OK; 404 Not Found
     res.send(`GET /team/${req.params.id} received`);
     console.log(req.params.id);
   })
   .post(function (req, res) { // add
     // res.send(`POST /team received`);
+    res.status(201);  // 201 Created; 401 Unauthorised; 409 Conflict; 404 Not Found 
     res.send(`POST /team {${req.body.id}, ${req.body.name}} received`);
     console.log(req.body);
   })
   .put(function (req, res) { // edit
+    res.status(200); // 200 OK; 401 Unauthorised; 404 Not Found 
     res.send(`PUT /team/${req.params.id} {${req.body.id}, ${req.body.name}} received`);
     console.log(req.body);
   })
   .delete(function (req, res) { // delete
+    res.status(200); // 200 OK; 401 Unauthorised; 404 Not Found
     res.send(`DELETE /team/${req.params.id} received`);
     console.log(req.params.id);
   });  
